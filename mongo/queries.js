c9603716cd31b50f1015ea60ba57381bec713272
BuildTools <unconfigured@null.spigotmc.org>
2021-11-12 12:40:16 -0500

    re-upload

---
+++ mongo/queries.js
@@ -0,0 +1,78 @@
+const mongoClient = require("./config");
+
+async function findListing1(response)
+{
+  console.log('Find Listing');
+  let connection = await mongoClient.connect()
+  console.log('Connected');
+  let db = await connection.db('sample_airbnb');
+  console.log('open airbnb databse')
+
+  let listingsAndReviews = await db.collection('listingsAndReviews')
+  console.log('select listings and reviews')
+
+  let listing = await listingsAndReviews.findOne({})
+
+  console.log('Close db')
+
+  response.send(listing)
+  connection.close()  
+}
+
+async function findListing2 (response)
+{
+  try{
+    var connection = await mongoClient.connect()
+    let db = await connection.db('sample_airbnb');
+    let listingsAndReviews = await db.collection('listingsAndReviews')
+    let listing = await listingsAndReviews.findOne({})
+    response.send(listing)
+  }
+  catch(error)
+  {
+    console.log(error)
+    response.send(error)
+  }
+  finally{
+    // This is where any cleanup code goes
+    connection.close()
+  }
+}
+
+async function findListing3(response,criteria)
+{
+  mongoClient.connect()
+    .then(connection=>connection.db('sample_airbnb'))
+    .then(db=>db.collection('listingsAndReviews'))
+    .then(listingsAndReviews=>listingsAndReviews.findOne(criteria))
+    .then(listing=>response.send(listing))
+    .catch(error => console.log(error))
+
+}
+
+
+async function findListings (response, criteria)
+{
+  try{
+    var connection = await mongoClient.connect()
+    let db = await connection.db('sample_airbnb');
+    let listingsAndReviews = await db.collection('listingsAndReviews')
+    let cursor = await listingsAndReviews.find(criteria).limit(4)
+    let listings = await cursor.toArray()
+    response.render('listings', {listings})
+  }
+  catch(error)
+  {
+    console.log(error)
+    response.send(error)
+  }
+  finally{
+    // This is where any cleanup code goes
+    connection.close()
+  }
+}
+
+let findListing = findListing3
+
+// Shortcut for {findListing:findListing;findListings:findListings}
+module.exports = {findListing,findListings}  
---
+++ routes/airbnb.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var mongoClient = require('./../mongo/config')
+var mongoQueries = require('./../mongo/queries')
+var router = express.Router();
+
+
+// localhost:3000/airbnb
+router.get('/', (req, res) => {
+  res.render('airbnb', {title:'AirBnb', mongoHost:mongoClient.options.srvHost});
+});
+
+/* GET users listing. 
+  localhost:3000/airbnb/send
+*/
+
+
+router.get('/send', (req,res)=>{
+
+  console.log(req.query)
+
+  parseInt
+  let criteria = 
+    {   bedrooms:{$gte:  parseInt(req.query.bedrooms)}, 
+        number_of_reviews:{$gte:50}, 
+        "address.country_code" : "US" ,
+         amenities : {$in : ['Wifi', 'Coffee maker' ]  }
+      }
+
+  mongoQueries.findListing(res,criteria);
+})
+
+router.get("/list", (req, res)=>{
+  let criteria = {}
+  mongoQueries.findListings(res, criteria)
+})
+
+
+
+module.exports = router;
